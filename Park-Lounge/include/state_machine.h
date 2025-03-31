c857280fed3487553f90086eea2b188ec861b2e4
paal <paal@example.com>
2025-03-31 08:45:01 +0200

    updated state machine

--- Park-Lounge/include/state_machine.h
+++ Park-Lounge/include/state_machine.h
@@ -11,6 +11,7 @@ typedef enum
     STATE_CHECKING_CSV, // Kennzeichen wird mit CSV (Whitelist) abgeglichen
     STATE_APPROVED,     // Kennzeichen ist gültig -> Schranke öffnet
     STATE_DENIED,       // Kennzeichen ungültig -> CSV (Blacklist) wird geschrieben
+    STATE_ERROR,        // Kennzeichenerkennung nicht möglich
     STATE_MANUAL_OPEN,  // Button gedrückt -> Schranke öffnet
     STATE_CLOSING,      // Schranke schließt
 } ParkhausState;
@@ -18,13 +19,14 @@ typedef enum
 // Übergänge
 typedef enum
 {
-    CAR_DETECTED,   // Kamera erkennt Auto
-    PLATE_SCANNED,  // Nummernschild wurde gescannt
-    PLATE_VALID,    // Nummernschild ist gültig
-    PLATE_INVALID,  // Nummernschild ist ungültig
-    BUTTON_PRESSED, // Manueller Button wurde gedrückt
-    WAITING,        // Warten bis Schranke schließt
-    DONE            // Übergang zurück zu IDLE
+    CAR_DETECTED,      // Kamera erkennt Auto
+    PLATE_SCANNED,     // Nummernschild wurde gescannt
+    PLATE_NOT_SCANNED, // Nummernschild konnte nicht gescannt werden
+    PLATE_VALID,       // Nummernschild ist gültig
+    PLATE_INVALID,     // Nummernschild ist ungültig
+    BUTTON_PRESSED,    // Manueller Button wurde gedrückt
+    WAITING,           // Warten bis Schranke schließt
+    DONE               // Übergang zurück zu IDLE
 } Transition;
 
 // *** Funktionsprototypen für die Zustandsmaschine ***
